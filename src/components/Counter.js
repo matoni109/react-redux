d658d512ac20a3e91cbce0ed02e867ea927a52e4
Chris Wade <chris@wadespace.net>
2021-05-11 16:33:45 +1000

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
-import { useSelector, useDispatch } from "react-redux";
+import { Component } from "react";
+import { useSelector, useDispatch, connect } from "react-redux";
 
 import classes from "./Counter.module.css";
 
@@ -28,5 +29,47 @@ const Counter = () => {
     </main>
   );
 };
-
 export default Counter;
+
+// CLASS BASED
+
+// class Counter extends Component {
+//   upHandler = () => {
+//     this.props.up();
+//   };
+
+//   downHandler = () => {
+//     this.props.down();
+//   };
+
+//   toggleCounterHandler = () => {};
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.upHandler.bind(this)}>Up</button>
+//           <button onClick={this.downHandler.bind(this)}>Down</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     up: () => dispatch({ type: "up" }),
+//     down: () => dispatch({ type: "down" }),
+//   };
+// };
+
+// export default connect()(Counter);
