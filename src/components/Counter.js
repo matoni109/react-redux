4d0062e6d70c8dc937a519ae33d2865609749b60
Chris Wade <chris@wadespace.net>
2021-05-11 17:02:27 +1000

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,8 +6,11 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const showDivToggle = useSelector((state) => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   // logic handler
   const upHandler = () => {
     dispatch({ type: "up" });
@@ -27,12 +30,16 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={upHandler}>Up</button>
-        <button onClick={increaseHandler}>Up by 5</button>
-        <button onClick={downHandler}>Down</button>
-      </div>
+      {showDivToggle && (
+        <>
+          <div className={classes.value}>{counter}</div>
+          <div>
+            <button onClick={upHandler}>Up</button>
+            <button onClick={increaseHandler}>Up by 5</button>
+            <button onClick={downHandler}>Down</button>
+          </div>
+        </>
+      )}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,32 @@
 import { createStore } from "redux";
 
-const couterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const couterReducer = (state = initialState, action) => {
   //  // return new state
   if (action.type === "up") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "down") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  // toggle reducer
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
   // return old state
