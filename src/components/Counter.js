e4e78bafdf7dd554dd149f02f594eb16f59fc5d7
Chris Wade <chris@wadespace.net>
2021-05-11 16:40:09 +1000

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,13 @@ const Counter = () => {
     dispatch({ type: "up" });
   };
 
+  const increaseHandler = () => {
+    dispatch({
+      type: "increase",
+      amount: 5,
+    });
+  };
+
   const downHandler = () => {
     dispatch({ type: "down" });
   };
@@ -23,6 +30,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={upHandler}>Up</button>
+        <button onClick={increaseHandler}>Up by 5</button>
         <button onClick={downHandler}>Down</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,11 @@ const couterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter + 1,
     };
   }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
   if (action.type === "down") {
     return {
       counter: state.counter - 1,
