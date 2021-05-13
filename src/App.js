bd96e1a3fe9583be3e1b8330333df040bd476e22
Chris Wade <chris@wadespace.net>
2021-05-13 15:48:35 +1000

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
 import React from "react";
+import UserProfile from "./components/UserProfile";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../src/store/index";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const loginState = useSelector((state) => state.auth.isAuthenticated);
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.login());
+  };
+
+  const logOutHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.logOut());
+  };
+
   return (
     <>
       <Header />
-      <Auth />
+      {loginState && <UserProfile />}
+      {!loginState && <Auth loginHandler={loginHandler} />}
       <Counter />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,41 @@
-import classes from './Header.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/index";
+
+import classes from "./Header.module.css";
 
 const Header = () => {
+  // setup dispatch and get the states from REDUX
+  const dispatch = useDispatch();
+  const loginState = useSelector((state) => state.auth.isAuthenticated);
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.login());
+  };
+
+  const logOutHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.logOut());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
+          {loginState && (
+            <>
+              <li>
+                <a href="/">My Products</a>
+              </li>
+              <li>
+                <a href="/">My Sales</a>
+              </li>
+            </>
+          )}
           <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
+            {!loginState && <button onClick={loginHandler}>Login</button>}
+            {loginState && <button onClick={logOutHandler}>Log Out</button>}
           </li>
         </ul>
       </nav>
